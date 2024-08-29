86a137b934d23a9a27f257869d25c892efa56452
Dreamerc1 <chelsreid99@gmail.com>
2024-08-29 20:45:59 +0100

    adding interface

--- Currencyconverter.java
+++ Currencyconverter.java
@@ -1,3 +1,9 @@
 public interface Currencyconverter {
 
-}
+        double convertCurrency(String sourceCurrency, String destinationCurrency, double amount);
+      
+        String[] getSupportedCurrencies();
+      
+        double getExchangeRate(String sourceCurrency, String destinationCurrency);
+      }
+
